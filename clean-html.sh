0501f8c704073f32c2c8b3cbc29b0bfd9fb619b8
nicolas <nicolas@gmail.com>
2023-08-22 10:16:33 -0300

    Agrego extensión .html

---
+++ clean-html.sh
@@ -0,0 +1,8 @@
+# Moverme a ~/Downloads
+cd ~/Downloads
+
+# Armar un directorio donde vamos a guardar las cosas
+mkdir -p pdf
+
+# Mover archivos pdf
+mv *.pdf ~/Escritorio/Universidad/Programación1/taller_bash/clean-files/archivos_temp/pdf
